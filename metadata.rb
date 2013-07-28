cd5af3f014b28f01d8321f757b2ba9fc2bc34909
Douglas F Shearer <dougal.s@gmail.com>
2013-07-28 12:48:28 +0100

    Fix dumb typo.

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      "Manages Solaris zpools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-supports %[solaris2 ubuntu]
+supports %w[solaris2 ubuntu]
